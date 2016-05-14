9921d0af2bb3677c9cd63f69c423c23525949e0b
m-pilia <martino.pilia@gmail.com>
2016-05-14 18:35:06 +0200

    read/write mesh files

--- support.jl
+++ support.jl
@@ -52,9 +52,126 @@ function readArray(filename, first=0, last=0; ty=Float64)
 end
 
 #==
- = Create a vectorized version of the argument function. The resulting function 
- = returns a scalar when applied to a single point (vector), a vector when 
- = applied to a vector of points (matrix).
+ = Read a mesh from a file.
+ =
+ = @param filename Name of the file for the mesh.
+ = @return A tuple (P, T, H, N3, N4), where P is a matrix containing vertices'
+ =         coordinates, T is a matrix containing tetrahedral connectivity,
+ =         H is a matrix containing Hexahedral connectivity, N3 contains the
+ =         connectivity of the triangular portion of the boundary, and N4 
+ =         contains the connectivity of the quadrilateral portion of the
+ =         boundary.
+ =#
+function readMesh(filename)
+    f = nothing;
+    try
+        f = open(filename);
+    catch
+        error("I/O error, cannot open $filename");
+    end
+    lines = readlines(f);
+    close(f);
+
+    # read a connectivity list from f
+    function readMatrix(name, w)
+        A = [];
+        i = findfirst(lines, "$name\n");
+        if i > 0
+            k = parse(Int64, lines[i+1]);
+            A = Matrix{Int64}(k, w);
+            for j in 1:k
+                A[j,:] = readdlm(IOBuffer(lines[i+1+j]), Int64)[1:w];
+            end
+        end
+        return A;
+    end
+    
+    # read dimension
+    i = findfirst(s -> match(r"^Dimension\s", s) != nothing, lines);
+    if i == 0
+        error("Dimension not found in file content");
+    end
+    m = match(r"^Dimension[ \t]*\n?([0-9]+)", string(lines[i], lines[i+1]));
+    d = parse(Int64, m[1]);
+
+    # read vertices number
+    i = findfirst(lines, "Vertices\n");
+    if i == 0
+        error("Vertices number not found in file content");
+    end
+    n = parse(Int64, lines[i+1]);
+
+    # read vertices data 
+    P = Matrix{Float64}(n, d);
+    for j in 1:n
+        P[j,:] = readdlm(IOBuffer(lines[i+1+j]))[1:d];
+    end
+    
+    # read connectivity lists
+    N3 = readMatrix("Triangles", 3);
+    N4 = readMatrix("Quadrilaterals", 4);
+    T  = readMatrix("Tetrahedra", 4);
+    H  = readMatrix("Hexahedra", 8);
+    
+    return P, T, H, N3, N4;
+end
+
+#==
+ = Write a mesh into file.
+ =
+ = @param filename Name for the output file.
+ = @param P        Vertices' coordinates.
+ = @param T        Tetrahedral connectivity.
+ = @param H        Hexahedral connectivity.
+ = @param N3       Triangle connectivity.
+ = @param N4       Quadrilateral connectivity.
+ =#
+function writeMesh(filename, P, T, H, N3, N4)
+    f = nothing;
+    try
+        f = open(filename, "w");
+    catch
+        error("I/O error, cannot open $filename");
+    end
+
+    # write a connectivity list into f
+    function writeMatrix(name, A)
+        if !isempty(A)
+            write(f, "$name\n$(height(A))\n");
+            for i in 1:height(A)
+                write(f, join(A[i,:], " "), " 1\n");
+            end
+        end
+    end
+
+    # head and vertices
+    write(f, 
+        """MeshVersionFormatted 1
+           Dimension
+           $(width(P))
+           Vertices
+           $(height(P))
+           """);
+    for i in 1:height(P)
+        write(f, join(P[i,:], " "), " 1\n");
+    end
+    
+    # connectivity lists 
+    writeMatrix("Triangles", N3);
+    writeMatrix("Quadrilaterals", N4);
+    writeMatrix("Tetrahedra", T);
+    writeMatrix("Hexahedra", H);
+
+    # eof
+    write(f, "End\n\n");
+
+    close(f);
+end
+
+#==
+ = Create a vectorized version of the argument scalar/vector valued function. 
+ = The resulting function returns a scalar/vector when applied to a single 
+ = point (vector), a vector/matrix when applied to a vector of points (matrix).
  =
  = @param f Single argument function to vectorize.
  = @return Vectorized version of f.
